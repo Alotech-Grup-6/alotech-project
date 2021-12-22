6b9db4d7d93e446b2f22eb880d6d53ac365ff5df
Busra <busraabiner@gmail.com>
2021-12-22 20:19:35 +0300

    consumer-button-add

--- sso-consumer/client/src/App.js
+++ sso-consumer/client/src/App.js
@@ -1,7 +1,24 @@
-export default function App() {
-  return (
-    <>
-      <h1>Sso-Consumer </h1>
-    </>
-  );
+import React from 'react';
+import './App.css'
+
+function App() {
+    let isAccesTokenValid = false;    
+
+    const onSubmit = (e) => {
+        if(isAccesTokenValid === true) {
+            window.location.href='http://localhost:8000';
+        }
+        else{
+            window.location.href='http://localhost:3050'
+        }
+    }
+
+    return (
+        <div>
+            <button onClick={onSubmit} className="button">LOGIN</button>
+        </div>
+    )
 }
+
+export default App
+
---
+++ sso-consumer/client/src/components/userInfo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function userInfo() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default userInfo
