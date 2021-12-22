be08f20b6debc69094e7e49a044e482a7a539776
ilkay murat ertekin <iertekinx@gmail.com>
2021-12-22 14:35:11 +0300

    Add createUser

--- user-manager/server/router/userRouter.js
+++ user-manager/server/router/userRouter.js
@@ -4,4 +4,6 @@ const router = express.Router();
 const userController =require('../controller/userController')
 
 router.route('/get-users').get(userController.getListOfUsers)
+router.route('/create').post(userController.createUser)
+
 module.exports = router;
