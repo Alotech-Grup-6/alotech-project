96d1c4d67d135bfc2f460b7b5090c44b94055b13
Emre Pala <emrpla12@gmail.com>
2021-12-26 16:27:54 +0300

    minor update

--- user-manager/server/router/userRouter.js
+++ user-manager/server/router/userRouter.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 
 const userController = require("../controller/userController");
-<<<<<<< HEAD
 
 router.route("/get-users").get(userController.getListOfUsers);
 router.route("/create").post(userController.createUser);
@@ -11,12 +10,3 @@ router.route("/get-user").get(userController.getUser);
 router.route("/update-user").put(userController.updateUser);
 
 module.exports = router;
-
-
-=======
-
-router.route("/get-users").get(userController.getListOfUsers);
-router.route("/create").post(userController.createUser);
-
-module.exports = router;
->>>>>>> f1cd7c8abd23bbbb0871de5fad23367115510d58
