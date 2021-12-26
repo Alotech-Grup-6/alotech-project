f8de45248a11d2e13527b5a9527f5c6b3864d4e1
ilkay murat ertekin <iertekinx@gmail.com>
2021-12-27 02:01:39 +0300

    Add manager-server Router validToken

--- user-manager/server/router/userRouter.js
+++ user-manager/server/router/userRouter.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const userController = require("../controller/userController");
+const validToken=require("../middleware/validToken")
 
-router.route("/get-users").get(userController.getListOfUsers);
-router.route("/create").post(userController.createUser);
+router.route("/get-users").get(validToken,userController.getListOfUsers);
+router.route("/create").post(validToken,userController.createUser);
 
 module.exports = router;
