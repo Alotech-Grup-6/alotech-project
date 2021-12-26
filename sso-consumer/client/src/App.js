d6068895dc4457cd636b7354b2e24bbdf1c60e25
ilkay murat ertekin <iertekinx@gmail.com>
2021-12-27 01:51:07 +0300

    Add consumer client getUser

--- sso-consumer/client/src/App.js
+++ sso-consumer/client/src/App.js
@@ -4,8 +4,30 @@ import Cookies from "js-cookie";
 
 export default function App() {
   const [url, setUrl] = useState(window.location.origin);
+  const [userName, setUserName] = useState("");
+  const [userSurname, setUserSurname] = useState("");
+  const [userType, setUserType] = useState("");
 
   const cookie = Cookies.get("token");
+
+  const getUser = async () => {
+    const res = await axios.get("http://localhost:3100/get-user", {
+      headers: {
+        Authorization: "Bearer " + cookie,
+      },
+      params: {
+        user_id: 98,
+      },
+    });
+    if (res.data.message === "invalid token") {
+      getToken();
+    } else {
+      const data = res.data.result[0];
+      setUserName(data.user_name);
+      setUserSurname(data.user_surname);
+      setUserType(data.user_type);
+    }
+  };
 
   const getToken = async () => {
     window.location.href = `http://localhost:3050/?redirectURL=${url}`;
@@ -16,15 +38,17 @@ export default function App() {
       url: url,
       token: cookie,
     });
-    console.log(res.data);
+
     if (res.data.message !== "token Validated") {
       await getToken();
+    } else {
+      getUser();
     }
   };
 
   useEffect(async () => {
     cookie === undefined ? await getToken() : await checkToken();
-  }, []);
+  });
 
   return (
     <>
