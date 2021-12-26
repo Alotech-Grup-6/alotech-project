7ce609b2ad0cce8b723b8fe315fcc0a387a0c8e9
ilkay murat ertekin <iertekinx@gmail.com>
2021-12-27 01:53:46 +0300

    Add manager-client getToken

--- user-manager/client/src/App.js
+++ user-manager/client/src/App.js
@@ -1,4 +1,15 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Cookies from "js-cookie";
+
 export default function App() {
+  const [url, setUrl] = useState(window.location.origin);
+  const [isAdmin, setIsAdmin] = useState(false);
+  const cookie = Cookies.get("token");
+
+  const getToken = async () => {
+    window.location.href = `http://localhost:3050/?redirectURL=${url}`;
+  };
   return (
   <>
   <h1>User Manager</h1>
