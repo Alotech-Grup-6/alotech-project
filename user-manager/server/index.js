e46a0bd471ba8e2d4a6079e423ab1369d90c56bb
Busra <busraabiner@gmail.com>
2021-12-28 21:04:09 +0300

    Add Log4js

--- user-manager/server/index.js
+++ user-manager/server/index.js
@@ -1,25 +1,59 @@
-const express = require("express")
-require('dotenv').config()
-const cors =require("cors")
-const app = express();
-const port=process.env.PORT
+const express = require("express");
+const log4js = require("log4js");
+const dbconn = require("./dbconfig");
 
-const userRouter=require('./router/userRouter')
+require("dotenv").config();
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT;
+
+const userRouter = require("./router/userRouter");
+const { json } = require("express");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(
-    cors({
-      origin: "*",
-    })
-  );
-  
+  cors({
+    origin: "*",
+  })
+);
 
-app.use("/",userRouter)
+const logger = log4js.getLogger();
 
+log4js.addLayout("json", function (config) {
+  return function (logEvent) {
+    console.log(logEvent.data[0]);
+    var sqlQuery = `INSERT INTO logs (information, data) values ('${logEvent.level.levelStr}', '${logEvent.data}')`;
 
+    dbconn.query(sqlQuery, function (err) {
+      if (err) console.log(err);
+      console.log("eklendi");
+    });
+    return (
+      JSON.stringify(logEvent.data) +
+      config.separator +
+      [logEvent.level.levelStr]
+    );
+  };
+});
 
-app.listen(port,()=>{
-    console.log('server started',port)
-})
+log4js.configure({
+  appenders: {
+    out: {
+      type: "stdout",
+      layout: { type: "json", separator: ",", pattern: "%d %p %c %X" },
+    },
+  },
+  categories: {
+    default: { appenders: ["out"], level: "all" },
+  },
+});
+
+app.use(log4js.connectLogger(logger, { level: "auto" }));
+
+app.use("/", userRouter);
+
+app.listen(port, () => {
+  console.log("server started", port);
+});
