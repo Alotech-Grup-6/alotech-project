d226da006d699d01e9d768b721e21da07490e253
ilkay murat ertekin <iertekinx@gmail.com>
2021-12-28 15:20:01 +0300

    Add Admin page user list and delete user

--- user-manager/client/src/components/AdminPage.js
+++ user-manager/client/src/components/AdminPage.js
@@ -1,36 +1,38 @@
-import React from 'react';
-import './AdminPage.css';
+import "./AdminPage.css";
 
-function AdminPage({users}) {
-
-  
+function AdminPage({ users, delUser }) {
   return (
     <div>
       <div>
-        
-        <h1 className='title'>Welcome to Admin Page</h1>
+        <h1 className="title">Welcome to Admin Page</h1>
       </div>
-      
-      <div className='cards'>
-        
-      {users.map((user=>(
-        <div className='card'>
-          <div className='card-body'>
-            <div className='photo'>
-              <img src='/images/avatar.png' className='card-img' alt='' />
-            </div>
-            <h1>{user.username}</h1>
-            <p>{user.user_name}</p>
-            <p>{user.user_surname}</p>
-            <div className='buttons'>
-              <button className='bn632-hover bn24'>Delete</button>
-              <button className='bn632-hover bn24'>Update</button>
+
+      <div className="cards">
+        {users.map((user) => (
+          <div key={user.user_id} className="card">
+            <div className="card-body">
+              <div className="photo">
+                <img src="/images/avatar.png" className="card-img" alt="" />
+              </div>
+              {user.user_id}
+              <h1>{user.username}</h1>
+              <p>{user.user_name}</p>
+              <p>{user.user_surname}</p>
+              <div className="buttons">
+                <button
+                  className="bn632-hover bn24"
+                  onClick={() => {
+                    delUser(user.user_id);
+                  }}
+                >
+                  Delete
+                </button>
+                <button className="bn632-hover bn24">Update</button>
+              </div>
             </div>
           </div>
-        </div>
-      )))}
+        ))}
       </div>
-      
     </div>
   );
 }
