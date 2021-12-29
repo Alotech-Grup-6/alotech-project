6c5479f56f3e6ce1b61911fe262d0c350c0b3311
ilkay murat ertekin <iertekinx@gmail.com>
2021-12-29 03:13:28 +0300

    Fix dblogger new path

---
+++ sso-auth/server/dblogger.js
@@ -0,0 +1,31 @@
+const dbconn = require("./dbconfig");
+const log4js = require("log4js");
+
+const logger = log4js.getLogger();
+
+log4js.addLayout("json", function (config) {
+  return function (logEvent) {
+    const sqlQuery = `INSERT INTO logs (information, data) values ('${logEvent.level.levelStr}', '${logEvent.data}')`;
+
+    dbconn.query(sqlQuery, function (err) {
+      if (err) {
+        return console.log("error");
+      }
+    });
+  };
+});
+
+log4js.configure({
+  appenders: {
+    out: {
+      type: "stdout",
+      layout: { type: "json", separator: ",", pattern: "%d %p %c %X" },
+    },
+  },
+  categories: {
+    default: { appenders: ["out"], level: "all" },
+  },
+});
+
+const loggers = log4js.connectLogger(logger, { level: "auto" });
+module.exports = loggers;
